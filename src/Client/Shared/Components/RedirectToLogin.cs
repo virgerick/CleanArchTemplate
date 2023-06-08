1651b0ca12c5ab9f00850a351404a8c78dc3644b
Heriberto Alcantara Luna <heriberto.alcantara@uteco.edu.do>
2023-06-08 18:03:58 +0200

    setup authorization with login

---
+++ src/Client/Shared/Components/RedirectToLogin.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace CleanArchTemplate.Client.Shared.Components;
+
+public partial class RedirectToLogin:ComponentBase
+{
+    protected  void OnInitialized()
+    {
+        _navigationManager.NavigateTo("/login");
+        base.OnInitialized();
+    }
+}
+
