89e53948170a8d2ff824cb955833f0fd7f6d0981
Heriberto Alcantara Luna <heriberto.alcantara@uteco.edu.do>
2023-06-19 14:42:55 +0200

    guid keys

--- src/Shared/Constants/Storage/StorageConstants.cs
+++ src/Shared/Constants/Storage/StorageConstants.cs
@@ -6,15 +6,15 @@ public static class StorageConstants
 {
     public  class Local: StorageKeys<Local>
     {
-        public static string Preference = "58>Mv/[q*bw,";
-        public static string LoggedUserData = "by3+Cdg*e*";
-        public static string AuthToken = "6*qJjaJsTG";
-        public static string RefreshToken = ".a{qW)tn+W";
-        public static string UserImageURL = "s{My5/$K;P";
+        public static string Preference = "3abeb8ae-0e0d-49e2-a024-950833da5ed5";
+        public static string LoggedUserData = "83243f7e-aec2-47b3-86d1-936106d3234f";
+        public static string AuthToken = "295030fc-8f19-45e6-9d81-d7218ae754ce";
+        public static string RefreshToken = "85a6eaee-6f76-43f2-b24f-79dedf62da58";
+        public static string UserImageURL = "2781de17-7f82-4a71-9c89-6b4968a1dae6";
     }
     public  class Server: StorageKeys<Server>
     {
-        public static string Preference = "e2d}^CCQ29";
+        public static string Preference = "d823d9af-f558-46e0-8316-cdea709a49a7";
     }
 }
 public class StorageKeys<T>
